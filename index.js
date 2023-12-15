5e6311b20a1a266b6eae1e8f6bbafbd2beb30b76
+dennis.mutua@student.moringaschool.com <dennis@DESKTOP-Q9A0DBN.localdomain>
2023-12-15 03:01:48 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+ customerName ='BOB';
+}
+function setBestCustomer(){
+     bestCustomer = 'not bob'
+   
+}
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob'
+}
+function changeLeastFavoriteCustomer(bob){
+   try{
+    const leastFavoriteCustomer = 'bob'
+
+   }catch(error){console.error()}
+}
